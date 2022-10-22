394c17df657087a91eac69a11a78cc648c703a3f
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-10-22 04:35:26 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,19 +5,20 @@ int main(void) {
 	BinTree_Init(&tree1, 20);
 	
 	//check root!
-	printf("location '%s' = %i\n", "0", BinTree_getDataAt(&tree1, "0"));
+	printf("location '0' = %i\n", BinTree_getDataAt(&tree1, "0"));
 
 	//add some nodes!
-	if(BinTree_addNodeAt(&tree1, "01", 10) == BT_OK)
+	if(BinTree_addNodeAt(&tree1, "01", 10) == BT_OK) {
 		puts("Added child.");
+	}
 	
-	if(BinTree_addNodeAt(&tree1, "02", 30) == BT_OK)
+	if(BinTree_addNodeAt(&tree1, "02", 30) == BT_OK) {
 		puts("Added child.");
+	}
 
 	//get those nodes!
-	printf("location '%s' = %i\n", "01", BinTree_getDataAt(&tree1, "01"));
-	printf("location '%s' = %i\n", "02", BinTree_getDataAt(&tree1, "02"));
-
+	printf("location '01' = %i\n", "01", BinTree_getDataAt(&tree1, "01"));
+	printf("location '02' = %i\n", "02", BinTree_getDataAt(&tree1, "02"));
 	BinTree_Free(&tree1);
 	return 0;
 }
